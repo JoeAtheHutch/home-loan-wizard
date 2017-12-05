2e5f87fe62eb79d07af91ee0a2e4a9389bb1070d
Joseph Hutch <jhutch38@gmail.com>
2017-12-04 17:04:12 -0700

    automation

--- nightwatch/test_data/css_selectors.js
+++ nightwatch/test_data/css_selectors.js
@@ -1,6 +1,7 @@
 module.exports = {
     homeScreen: {
-        startButton: 'button[class=home-btn]'
+        startButton: 'button[class=home-btn]',
+        
     },
     loanPropTypeScreen: {
         loanDropDown: 'select[name=loanDropDown]',
@@ -18,5 +19,54 @@ module.exports = {
             optionMobile: 'option[name=mobileHome]'
         },
         nextButton: 'button[name=next]'
+    },
+    cityScreen: {
+        city: 'input[name="cityName"]'
+    },
+    whatPropScreen: {
+        primaryHome: 'button[name="primary"]',
+        rental: 'button[name="rental"]',
+        secondHome: 'button[name="secondary"]'        
+    },
+    foundHomeScreen: {
+        found: 'button[name="yesFound"]',
+        notFound: 'button[name="noFound"]'
+    },
+    agentScreen: {
+        agent: 'button[name="yesAgent"]',
+        noAgent: 'button[name="noAgent"]'
+    },
+    paymentScreen: {
+        purchasePrice: 'input[name="purchasePrice"]',
+        downPayment: 'input[name="downPayment"]',
+        nextButton: 'button[name=next]'
+    },
+    creditScoreScreen: {
+        scoreExcellent: 'button[name="excellent"]',
+        scoreGood: 'button[name="good"]',
+        scoreFair: 'button[name="fair"]',
+        scorePoor: 'button[name="poor"]'
+    },
+    historyScreen: {
+        noHistory: 'button[name="noBankruptForeclosure"]',
+        bankruptcy: 'button[name="bankrupt"]',
+        foreclusure: 'button[name="foreclosure"]',
+        bothHistory: 'button[name="bankruptForeclosure"]'
+    },
+    addressScreen: {
+        addressOne: 'input[name="addressOne"]',
+        addressTwo: 'input[name="addressTwo"]',
+        addressThree:  'input[name="addressThree"]',
+        nextButton: 'button[name=next]'        
+    },
+    nameScreen: {
+        firstName: 'input[name=firstName"]',
+        lastName: 'input[name="lastName"]',
+        email: 'input[name="email"]',
+        nextButton: 'button[name=next]'
+    },
+    overviewScreen: {
+        send: 'button[name="send"]',
+        startAgain: 'button[name="startOver"]'
     }
 }
---
+++ nightwatch/test_data/test_data_scratch.js
@@ -0,0 +1,18 @@
+Buttons:
+getStarted: 'button[class="home-btn"]',
+next: 'button[name="next"]', //Next button, repeated on several pages
+primaryHome: 'button[name="primary"]',
+rental: 'button[name="rental"],
+secondHome: 'button[name="secondary"],
+foundHome: 'button[name="yesFound"]',
+notFoundHome: 
+
+
+Text Fields:
+city: 'input[name="cityName"]',
+
+
+
+Dropdowns:
+typeOfLoan: 'select[name="loanDropDown"]',
+typeOfProperty: 'select[name="propDropDown"]',
